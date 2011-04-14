c5e74ddca7ed0a6d722a3ccda743315c5fa5e41c
Robert Miner <rminer@studioimac23.local>
2011-04-14 09:10:16 -0500

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.6'
+
+gem 'mysql2'
+
+# group :development, :test do
+#   gem 'webrat'
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WmApp::Application.config.secret_token = '1112ad34c148abc7b6d2144984041b76ef5ad6775c6b1cd1af6aafc9cc26bc1dc63e555ade9d7ec6de0fa00fe44c592b1dc252dc5d43df25559e322ed1a227d4'
