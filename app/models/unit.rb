7385e060fb6119a1d48b45bf2e6dee622e136d73
Mirai <robertrminer@gmail.com>
2011-04-19 10:01:52 -0500

    Removed feat association in unit and removed feat navigation link.

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,7 +2,6 @@ class Unit < ActiveRecord::Base
   belongs_to :faction
   belongs_to :unit_type
   has_and_belongs_to_many :weapons
-  has_one :feat
   has_and_belongs_to_many :spells
   has_and_belongs_to_many :warcasters
   has_and_belongs_to_many :warjacks
