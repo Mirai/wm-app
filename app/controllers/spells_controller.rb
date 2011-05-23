3a2a467dee6b45779fbdf658b1d63790cf539b2a
Mirai <robertrminer@gmail.com>
2011-05-23 15:51:05 -0500

    Creating or updating a spell sends you to the show page.  The show page for a spell was formatted like the spell list on a profile page.

--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -1,53 +1,53 @@
 class SpellsController < ApplicationController
   def new
     @spell = Spell.new
-    
+
     respond_to do |format|
       format.html
       format.js { render :partial => 'form' }
     end
   end
-  
+
   def create
     @spell = Spell.new(params[:spell])
-    
+
     if @spell.save
       respond_to do |format|
-        format.html { redirect_to spells_path, :notice => "Spell successfully created." }
+        format.html { redirect_to @spell, :notice => "Spell successfully created." }
         format.js
       end
     else
       render :action => 'new'
     end
   end
-  
+
   def edit
     @spell = Spell.find(params[:id])
   end
-  
+
   def update
     @spell = Spell.find(params[:id])
-    
+
     if @spell.update_attributes(params[:spell])
-      redirect_to spells_path, :notice => "Spell successfully updated."
+      redirect_to @spell, :notice => "Spell successfully updated."
     else
       render :action => 'edit'
     end
   end
-  
+
   def index
     @spells = Spell.order(:name).page(params[:page])
   end
-  
+
   def show
     @spell = Spell.find(params[:id])
   end
-  
+
   def destroy
     @spell = Spell.find(params[:id])
     @spell.destroy
-    
+
     redirect_to spells_path
   end
-    
+
 end
