518276397e5936ad1c9e3d9c8fcf8603ed9d00da
Mirai <robertrminer@gmail.com>
2011-05-24 13:22:20 -0500

    Ability to add a new order from the edit squad page.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,44 +1,52 @@
 class OrdersController < ApplicationController
   def new
     @order = Order.new
+
+    respond_to do |format|
+      format.html
+      format.js { render :partial => 'form' }
+    end
   end
-  
+
   def create
     @order = Order.new(params[:order])
-    
+
     if @order.save
-      redirect_to orders_path, :notice => "Order created successfully."
+      respond_to do |format|
+        format.html { redirect_to orders_path, :notice => "Order created successfully." }
+        format.js
+      end
     else
       render :action => 'new'
     end
   end
-  
+
   def edit
     @order = Order.find(params[:id])
   end
-  
+
   def update
     @order = Order.find(params[:id])
-    
+
     if @order.update_attributes(params[:order])
       redirect_to orders_path, :notice => "Order updated successfully."
     else
       render :action => 'edit'
     end
   end
-  
+
   def index
     @orders = Order.order(:name).page(params[:page])
   end
-  
+
   def show
     @order = Order.find(params[:id])
   end
-  
+
   def destroy
     @order = Order.find(params[:id])
     @order.destroy
-    
+
     redirect_to orders_path
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-jQuery.ajaxSetup({  
-	'beforeSend': function (xhr) {xhr.setRequestHeader("Accept", "text/javascript")}  
+jQuery.ajaxSetup({
+	'beforeSend': function (xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 });
 
 $.fn.ajaxUpdater = function(options) {
@@ -18,45 +18,46 @@ $.fn.ajaxUpdater = function(options) {
 
       $.get(this.href, function(data) {
         $($dom_id).html(data)
+		$($dom_id).children().find('input').not(':hidden').first().focus()
 		$($dom_id).append('<a id="cancel" href="javascript:void()">Cancel</a>')
       })
     })
   })
 }
 
-function remove_fields(link, class) {  
-	$(link).prev("input[type=hidden]").val("1");  
-    $(link).closest("." + class).hide();  
+function remove_fields(link, class) {
+	$(link).prev("input[type=hidden]").val("1");
+    $(link).closest("." + class).hide();
 }
 
-function add_fields(link, association, content) {  
-    var new_id = new Date().getTime();  
-    var regexp = new RegExp("new_" + association, "g");  
-    $(link).parent().before(content.replace(regexp, new_id));  
+function add_fields(link, association, content) {
+    var new_id = new Date().getTime();
+    var regexp = new RegExp("new_" + association, "g");
+    $(link).parent().before(content.replace(regexp, new_id));
 }
 
-$(document).ready(function() {	
+$(document).ready(function() {
 	//show and hide the extra ranged weapon fields
 	if($('#weapon_ranged').is(':checked')) {
 		$('#ranged_fields').css('display', 'block');
 	}
-	
-	$('#weapon_ranged').live('click', function() {		
+
+	$('#weapon_ranged').live('click', function() {
 		if($(this).is(':checked')) {
 			$('#ranged_fields').show("fast");
 		} else {
 			$('#ranged_fields').hide("fast");
 		}
 	})
-	
-	
+
+
 	//when the new weapon form is submitted on the unit page
 	$('#x_newweapon').each(function() {
 		$('#new_weapon').live('submit', function() {
 			$.post($(this).attr('action'), $(this).serialize(), null, "script");
 			return false;
 		})
-		
+
 		$('#cancel').live('click', function() {
 			$('#x_newweapon').html('');
 		})
@@ -68,57 +69,67 @@ $(document).ready(function() {
 			$.post($(this).attr('action'), $(this).serialize(), null, "script");
 			return false;
 		})
-		
+
 		$('#cancel').live('click', function() {
 			$('#x_newspell').html('');
 		})
 	})
-	
+
+	$('#x_neworder #new_order').live('submit', function() {
+		$('#add_order a').trigger('click')
+		$.post($(this).attr('action'), $(this).serialize(), null, "script");
+		return false;
+	})
+
+	$('#x_neworder #cancel').live('click', function() {
+		$('#x_neworder').html('');
+	})
+
 	//when add spell or add weapon submitted on unit page
 	$('.unit_addition').submit(function() {
 		$.post($(this).attr('action'), $(this).serialize(), null, "script");
 		return false;
 	})
-	
+
 	//when remove spell or weapon link is clicked
 	$('.remove').live('click', function() {
 		$.get($(this).attr('href'), null, null, "script");
 		return false;
 	})
-	
+
 	$('.remove').live('click', function() {
 		$(this).parent().remove();
 	})
-	
+
 	// show damage grid based on select
 	$('#unit_warjack_attributes_damage_grid_id').change(function() {
 		$.get('/damage_grids?id=' + $(this).val());
 		return false;
 	})
 	$('#unit_warjack_attributes_damage_grid_id').trigger('change')
-	
+
 	// order dropdown
 	$('.order_select').live('change', function() {
 		/*var optionsArr = "<option value=''>No parent</option>";
-		
+
 		$('.order_select option:selected').each(function () {
 			optionsArr += "<option value='" + $(this).val() + "'>" + $(this).text() + "</option>\n";
 		})
-		
+
 		$('.parent_select').html(optionsArr)*/
-		
+
 		var option = "<option value='" + $(this).val() + "'>" + $(this).children(":selected").text() + "</option>";
 		$('.parent_select').append(option);
 	})
-	
+
 	$('#add_order a').click(function() {
 		$('.order_select').trigger('change');
 	})
-	
+
 	$('.attachment_check').change(function() {
 		$('.attachment_check').is(':checked') ? $('#attachment_field').show("fast") : $('#attachment_field').hide("fast");
 	})
 	$('.attachment_check').trigger('change')
-		
+
 	$(function() { $('a[rel=updater]').ajaxUpdater() })
 });
