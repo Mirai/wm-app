287cb1d6f40d1055ac98f0c82b142b557e2867ca
Mirai <robertrminer@gmail.com>
2011-06-06 15:17:10 -0500

    Removed non parent units from list of solos, ie Darius' Halfjacks

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -3,59 +3,59 @@ class UnitsController < ApplicationController
     @unit = Unit.new
     @available_orders = UnitOrder.find(@unit.multi_orders)
   end
-  
+
   def create
     @unit = Unit.new(params[:unit])
-    
+
     if @unit.save
       redirect_to @unit, :notice => "Solo created successfully."
     else
       render 'new'
     end
   end
-  
+
   def edit
     @unit = Unit.find(params[:id])
     @available_orders = UnitOrder.find(@unit.multi_orders)
   end
-  
+
   def update
     @unit = Unit.find(params[:id])
-    
+
     if @unit.update_attributes(params[:unit])
       redirect_to @unit, :notice => "Solo updated successfully."
     else
       render 'edit'
     end
   end
-  
+
   def index
     if params[:id].nil?
       @warcasters = Unit.where("unit_type_id = 1").order(:short_name)
       @warjacks = Unit.where("unit_type_id = 2 OR unit_type_id = 3").order(:unit_type_id, :name)
       @troops = Squad.order(:name)
-      @solos = Unit.where("unit_type_id = 5").order(:name)
+      @solos = Unit.order(:name).find_all_by_unit_type_id_and_parent_id(5, nil)
     else
       @warcasters = Unit.where("unit_type_id = 1 AND faction_id = #{params[:id]}").order(:short_name)
       @warjacks = Unit.where("(unit_type_id = 2 OR unit_type_id = 3) AND faction_id = #{params[:id]}").order(:unit_type_id, :name)
       @troops = Squad.where("faction_id = #{params[:id]}").order(:name)
-      @solos = Unit.where("unit_type_id = 5 AND faction_id = #{params[:id]}").order(:name)
+      @solos = Unit.order(:name).find_all_by_unit_type_id_and_parent_id_and_faction_id(5, nil, params[:id])
     end
   end
-  
+
   def show
     @unit = Unit.find(params[:id])
     @available_weapons = Weapon.order(:name) - @unit.weapons
     @available_spells = Spell.order(:name) - @unit.spells
   end
-  
+
   def destroy
     @unit = Unit.find(params[:id])
     @unit.destroy
-    
+
     redirect_to(units_url)
   end
-  
+
   def add_weapon
     @equip = Equip.new
     @equip.unit_id = params[:unit][:id]
@@ -63,42 +63,42 @@ class UnitsController < ApplicationController
     @equip.hand = params[:equip][:hand]
     @equip.x2 = params[:equip][:x2]
     @equip.save
-    
+
     respond_to do |format|
       format.html { redirect_to @unit, :notice => "Weapon successfully added." }
       format.js
     end
   end
-  
+
   def add_spell
     @unit = Unit.find(params[:unit][:id])
     @spell = Spell.find(params[:unit][:spells])
     @unit.spells << @spell
-    
+
     respond_to do |format|
       format.html { redirect_to @unit, :notice => "Spell successfully added." }
       format.js
     end
   end
-  
+
   def remove_weapon
     @unit = Unit.find(params[:id])
     @removed_weapon = Equip.find_by_unit_id_and_weapon_id(params[:id], params[:weapon_id])
-    
+
     @unit.equips.delete(@removed_weapon)
-    
+
     respond_to do |format|
       format.html { redirect_to @unit, :notice => "Weapon successfully removed." }
       format.js
     end
   end
-  
+
   def remove_spell
     @unit = Unit.find(params[:id])
     @removed_spell = Spell.find(params[:spell_id])
-    
+
     @unit.spells.delete(@removed_spell)
-    
+
     respond_to do |format|
       format.html { redirect_to @unit, :notice => "Spell successfully removed." }
       format.js
